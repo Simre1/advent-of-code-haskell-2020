97254ff1590ee3c5ff641617d3409bb37973c80f
Simon Reitinger <simre4775@gmail.com>
2020-12-02 11:22:31 +0100

    Added Day2

--- src/Day1/Main.hs
+++ src/Day1/Main.hs
@@ -1,6 +1,7 @@
 module Day1.Main where
 
 
+import Criterion.Main
 solution1 :: IO ()
 solution1 = do
   input <- fmap (read @Int) . lines <$> readFile "inputs/day1/input1"
@@ -23,4 +24,6 @@ solution2 = do
           then [(x,y,z)]
           else []
   print $ let (a,b,c) = head results in a * b * c 
-  
+
+shortSolution :: IO ()
+shortSolution = readFile"inputs/day1/input1">>=(\x->print$head[a*b|a<-x,b<-x,a+b==2020]).fmap read.lines
---
+++ src/Day2/Main.hs
@@ -0,0 +1,46 @@
+module Day2.Main where
+
+import qualified Data.Attoparsec.Text as P
+import Data.Text (index, singleton,  Text, count)
+import Data.Text.Read ( decimal )
+import Data.Maybe (isJust)
+import qualified Data.Text.IO as T
+import Control.Applicative ( Alternative((<|>)) )
+
+inputParser :: P.Parser [(Int, Int, Char, Text)]
+inputParser = do
+  firstNumber <- P.takeWhile (/= '-')
+  P.char '-'
+  secondNumber <- P.takeWhile (/= ' ')
+  P.char ' '
+  char <- P.anyChar
+  P.char ':'
+  P.char ' '
+  password <- P.takeWhile (/='\n')
+  let result = either error id $ (,,,) 
+                <$> fmap fst (decimal firstNumber) 
+                <*> fmap fst (decimal secondNumber)
+                <*> pure char 
+                <*> pure password
+  P.char '\n' <|> pure 'a'
+  nextChar <- P.peekChar
+  (result:) <$> if isJust nextChar 
+    then inputParser
+    else pure []
+
+runSolution :: ((Int, Int, Char, Text) -> Bool) -> IO ()
+runSolution checkPassword = do
+  passwords <- either error id . P.parseOnly inputParser <$> T.readFile "inputs/day2/input1"
+  let validPasswords = foldr (\p a -> if checkPassword p then succ a else a) 0 passwords
+  print validPasswords
+
+solution1 :: IO ()
+solution1 = runSolution $ \(min, max, char, t) ->
+  let actual = count (singleton char) t
+  in min <= actual && actual <= max
+
+solution2 :: IO ()
+solution2 = runSolution $ \(min, max, char, t) ->
+  let res1 = t `index` pred min == char
+      res2 = t `index` pred max == char
+  in (res1 || res2) && not (res1 && res2)
