9638ff9e1c2098e31430cb67afd8f26f493f831f
Simon Reitinger <simre4775@gmail.com>
2020-12-15 17:53:41 +0100

    Faster day 15

--- src/Day15/Main.hs
+++ src/Day15/Main.hs
@@ -4,7 +4,9 @@ import qualified Data.HashMap.Strict as HM
 import Data.List.Split (splitOn)
 import Data.Foldable (Foldable(foldl'))
 import qualified Data.Array.MArray as A
+import Data.Word
 import qualified Data.Array.IO as A
+import Data.Bits
 
 import Control.DeepSeq ( force )
 
@@ -22,18 +24,24 @@ speakNumber (last, numbers) turn =
       newMap = HM.alter (pure . maybe (0,turn) (\(_,b) -> (b,turn))) newNumber numbers
   in force $ (newNumber, newMap)
 
-speakNumberMut :: A.IOArray Int (Int, Int) -> Int -> Int -> IO Int
+speakNumberMut :: A.IOUArray Word32 Word64 -> Word32 -> Word32 -> IO Word32
 speakNumberMut table turn last = do
-  (firstTurn,secondTurn) <- A.readArray table last
+  (firstTurn,secondTurn) <- word64To32 <$> A.readArray table last
   let newNumber = if firstTurn == 0 then 0 else secondTurn - firstTurn
-  (_,y) <- A.readArray table newNumber
-  A.writeArray table newNumber (y,turn)
+  (_,y) <- word64To32 <$> A.readArray table newNumber
+  A.writeArray table newNumber $ word32To64 y turn
   pure newNumber
 
 solution2 :: IO ()
 solution2 = do
-  numbers <- fmap (read @Int) . splitOn "," <$> readFile "inputs/day15/input1"
-  numberTable <- A.newArray (0,30000000) (0,0)
-  foldMap (uncurry (A.writeArray numberTable)) (zip numbers (repeat 0 `zip` [1..]))
-  lastNumber <- foldl' (\b a -> b >>= speakNumberMut numberTable a) (pure $ last numbers) [length numbers+1..30000000]
+  numbers <- fmap (read @Word32) . splitOn "," <$> readFile "inputs/day15/input1"
+  numberTable <- A.newArray (0,30000000) (0 :: Word64)
+  foldMap (uncurry (A.writeArray numberTable)) (zip numbers (uncurry word32To64 <$> repeat 0 `zip` [1..]))
+  lastNumber <- foldl' (\b a -> b >>= speakNumberMut numberTable a) (pure $ last numbers) [fromIntegral (length numbers+1)..30000000]
   print lastNumber
+
+word32To64 :: Word32 -> Word32 -> Word64
+word32To64 w1 w2 = shiftL (fromIntegral w1) 32 .|. fromIntegral w2
+
+word64To32 :: Word64 -> (Word32, Word32)
+word64To32 w = (fromIntegral $ shiftR w 32,fromIntegral w)
